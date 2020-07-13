92ea0be10e9d76114415bdfcb13a5569670fbf58
Neeraj Kumar <neeraj8007@hotmail.com>
2020-07-13 16:26:46 +0530

    Total order value after applying promotion types

---
+++ Product.cs
@@ -0,0 +1,34 @@
+namespace Promotion_Engine
+{
+    public class Product
+    {
+
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Product(string id)
+        {
+            this.Id = id;
+            switch (id)
+            {
+                case "A":
+                    this.Price = 50m;
+
+                    break;
+                case "B":
+                    this.Price = 30m;
+
+                    break;
+                case "C":
+                    this.Price = 20m;
+
+                    break;
+                case "D":
+                    this.Price = 15m;
+                    break;
+            }
+        }
+
+    }
+}
